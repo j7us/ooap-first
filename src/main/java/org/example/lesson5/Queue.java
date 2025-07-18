bc71dd0bf752bef290f85a0f30880c71e688ec3a
Egor Pozdnov <pozdner98@gmail.com>
2025-07-18 18:52:11 +0300

    Lesson 5

---
+++ src/main/java/org/example/lesson5/Queue.java
@@ -0,0 +1,25 @@
+package org.example.lesson5;
+
+public abstract class Queue<T> {
+
+    // Конструктор
+
+    public Queue() {}
+
+    // Команды
+
+    // постусловие: элемент добавлен в конец очереди.
+    public abstract void enqueue(T element);
+
+    // предусловие: очередь не пустая
+    // постусловие: из головы очереди взят элемент
+    public abstract T dequeue();
+
+
+    // Запросы
+
+    // предусловие: очередь не пустая
+    public abstract T peek();
+
+    public abstract int size();
+}
